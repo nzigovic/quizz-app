528e7429b82f3088eeba1da22987e4cb39923efb
nzigovic <nadirzigovic4@gmail.com>
2024-10-31 12:47:38 +0100

    ispravka greski zavrseni je sve

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const questions = [
     },{
         question: "Koliko je 7x7?", 
         answers: [
-            {text:"", correct:false},
+            {text:"22", correct:false},
             {text:"68", correct:false},
             {text:"63", correct:true},
             {text:"52", correct:false},
@@ -37,16 +37,17 @@ let score = 0;
 function startquiz() {
     currentQuestionIndex = 0;
     score = 0;
-    sledeceBtn.innerHTML = "Sledece";
+    sledeceBtn.innerHTML = "Sledeće";
+    sledeceBtn.style.display = "none";
     showQuestion();
 }
 
 function showQuestion() {
+    resetStat();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    odgovori.innerHTML = ""; 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
@@ -56,67 +57,54 @@ function showQuestion() {
             button.dataset.correct = answer.correct;
         }
         
-        button.addEventListener("click", selectAnswer)
-        });
+        button.addEventListener("click", selectAnswer);
+    });
 }
 
-
-function selectAnswer (e){
-    const selectedBtn = e.target ;
+function selectAnswer(e) {
+    const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect){
-        selectedBtn.classList.add ("correct");
-        score++ ;
-
-    }else {
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
         selectedBtn.classList.add("incorrect");
     }
-        Array.from(odgovori.children).forEach(button => {
-        if(button.dataset.correct === "true"){
+    Array.from(odgovori.children).forEach(button => {
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
-
         }
-        button.disabled = "true"
+        button.disabled = true;
     });
     sledeceBtn.style.display = "block";
 }
 
-sledeceBtn.addEventListener("click", ()=> {
-    if(currentQuestionIndex < questions.length){
+sledeceBtn.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length - 1) {
         handleNextButton();
+    } else {
+        showScore();
     }
-    else{
-        startquiz()
-    }
-}) 
+});
 
-
-function resetStat(){
-    sledeceBtn.style.display = "none"
-    while(odgovori.firstChild){
-        odgovori.removeChild(odgovori.firstChild)
+function resetStat() {
+    sledeceBtn.style.display = "none";
+    while (odgovori.firstChild) {
+        odgovori.removeChild(odgovori.firstChild);
     }
 }
 
-    function showQuestion(){
-        resetStat()
-        questionElement.innerHTML =    `Tvoj rezultat je ${score} od mogucih ${questions.length}!` ; 
-        sledeceBtn.innerHTML = "Ponovi";
-        sledeceBtn.style.display  = "block"
-    }
+function handleNextButton() {
+    currentQuestionIndex++;
+    showQuestion();
+}
 
+function showScore() {
+    resetStat();
+    questionElement.innerHTML = `Tvoj rezultat je ${score} od mogućih ${questions.length}!`;
+    sledeceBtn.innerHTML = "Ponovi";
+    sledeceBtn.style.display = "block";
+    sledeceBtn.onclick = startquiz;  
+}
 
-
- //   function handleNextButton (){
-   
- 
- //currentQuestionIndex++;
-   //     if(currentQuestionIndex < questions.length){
-     //       showQuestion();
-
-       // }else{
-         //   showQuestion()
-        //}
-    //}
-
-startquiz()
+startquiz();
